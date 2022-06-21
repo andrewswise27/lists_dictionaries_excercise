357c7a9343431402e39bea6d0daf3f6b97631023
Andrew Wise <andrewswise2795@gmail.com>
2022-06-21 17:32:41 +0100

    completed B8

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -74,9 +74,7 @@ for numbers in users["Avril"]["lottery_numbers"]:
     print(numbers)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"]["lottery_numbers"].append(7)
-print(users["Erik"]["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
 users["Erik"]["home_town"] = "Edinburgh"
-print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
