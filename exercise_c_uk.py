415a86a7cf7496ca68fab881d02a403734d6e5da
Andrew Wise <andrewswise2795@gmail.com>
2022-06-21 22:03:49 +0100

    committed c

--- exercise_c_uk.py
+++ exercise_c_uk.py
@@ -22,8 +22,8 @@ united_kingdom[1]["capital"] = "Cardiff"
 northern_ireland = {"name": "Northern Ireland", "population": 1811000, "capital": "Belfast"}
 united_kingdom.append(northern_ireland)
 # 3. Use a loop to print the names of all the countries in the UK.
-# for names in (united_kingdom["name"]):
-#   print(names)
+for names in united_kingdom:
+  print(names["name"])
 # 4. Use a loop to find the total population of the UK.
 population_total = 0
 for population in united_kingdom:
